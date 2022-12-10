2013d1ffa9b6ef5c43430eb9e7367b37888dab0b
Nadeem <nadeemlameec@gmail.com>
2022-12-11 03:01:28 +0530

    11-12-2022

--- main/default/lwc/contactCreator/contactCreator.js
+++ main/default/lwc/contactCreator/contactCreator.js
@@ -4,9 +4,10 @@ import CONTACT_OBJECT from '@salesforce/schema/Contact';
 import FNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import ID_FIELD from '@salesforce/schema/Contact.Account.name';
 export default class ContactCreator extends LightningElement {  
     objectApiName = CONTACT_OBJECT;
-    fields = [FNAME_FIELD,LNAME_FIELD,EMAIL_FIELD];
+    fields = [FNAME_FIELD,LNAME_FIELD,EMAIL_FIELD,ID_FIELD];
     handleSuccess(event) {
         const toastEvent = new ShowToastEvent({
             title: "Contact created",
@@ -15,4 +16,4 @@ export default class ContactCreator extends LightningElement {
         });
         this.dispatchEvent(toastEvent);
     }
-}
+} 
